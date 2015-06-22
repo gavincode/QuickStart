324d0d3f010a4604797e3c644204ba3021c667c6
gavin <709181180@qq.com>
2015-06-22 22:46:31 +0800

    bug

--- QuickStartUI/Model/FileInfos.cs
+++ QuickStartUI/Model/FileInfos.cs
@@ -28,8 +28,12 @@ namespace QuickStartUI
             FilePath = file;
             Crdate = File.GetLastAccessTime(file);
 
-            Icon = IconHandler.GetFileIcon(file);
-            if (Path.GetExtension(Name) == ".sln" && slnIcon != null) Icon = slnIcon;
+            if (Path.GetExtension(Name) == ".sln" && slnIcon != null)
+                Icon = slnIcon;
+            else
+                Icon = IconHandler.GetFileIcon(file);
+
+            ShowLable = new Label();
         }
 
         public Icon Icon { get; set; }
@@ -38,5 +42,6 @@ namespace QuickStartUI
         public String NameLetters { get; set; }
         public String FilePath { get; set; }
         public DateTime Crdate { get; set; }
+        public Label ShowLable { get; set; }
     }
 }
