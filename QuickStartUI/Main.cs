d587d0e640c37e69bd7b89ece9ad828dcb45fe54
gavin <709181180@qq.com>
2015-06-24 12:34:38 +0800

    remove useless using

--- QuickStartUI/Main.cs
+++ QuickStartUI/Main.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Windows.Forms;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace QuickStartUI
 {
--- QuickStartUI/Program.cs
+++ QuickStartUI/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Windows.Forms;
 using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading;
+using System.Windows.Forms;
 
 namespace QuickStartUI
 {
